51ff47025b7ebee0b5d5c519a326909d3a120f37
rafaeldnribeiro <rafaeldnribeiro@hotmail.com>
2021-08-08 17:31:14 -0300

    refactor

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -9,28 +10,12 @@ export class CourseListComponent implements OnInit{
 
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService){
+
+    }
+
     ngOnInit(): void{
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'xps 8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'November, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'lkl 1094',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'November, 4, 2019'
-            },
-        ]
+        this.courses = this.courseService.retriveAll();
+        
     }
 }
