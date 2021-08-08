88d821681a16fff48a4c99cb98418c25d37927db
rafaeldnribeiro <rafaeldnribeiro@hotmail.com>
2021-08-08 17:48:10 -0300

    feature

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -6,16 +6,31 @@ import { CourseService } from "./course.service";
     selector: 'app-course-list',
     templateUrl: './course-list.component.html',
 })
-export class CourseListComponent implements OnInit{
+export class CourseListComponent implements OnInit {
 
-    courses: Course[] = [];
+    filteredCourses: Course[] = [];
 
-    constructor(private courseService: CourseService){
+    _courses: Course[] = [];
+
+    _filterBy: string;
+
+    constructor(private courseService: CourseService) {
 
     }
 
-    ngOnInit(): void{
-        this.courses = this.courseService.retriveAll();
+    ngOnInit(): void {
+        this._courses = this.courseService.retriveAll();
+        this.filteredCourses = this._courses;
         
+    }
+
+    set filter(value: string) {
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1)
+
+    }
+
+    get filter() {
+        return this._filterBy;
     }
 }
