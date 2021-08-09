143015fbf1bf4647dd7952770ee15750c7f38944
rafaeldnribeiro <rafaeldnribeiro@hotmail.com>
2021-08-09 09:25:23 -0300

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,27 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './error404/error404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    CourseInfoComponent,
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot(
       [
-        {
-          path: 'courses', component: CourseListComponent
-        },
-        {
-          path: 'courses/info/:id', component: CourseInfoComponent
-        },
         {
           path: '', redirectTo: 'courses', pathMatch: 'full'
         },
