6446144eb31c0d93714f692dd3cf7f7d294f7293
rafaeldnribeiro <rafaeldnribeiro@hotmail.com>
2021-08-09 09:42:46 -0300

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
+import { StarComponent } from './shared/component/star/star.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './error404/error404.component';
